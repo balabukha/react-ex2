afc4814d8df5cc565465c524ee6e3345ac46df88
balabukha <3909591@gmail.com>
2017-05-19 18:28:43 +0300

    text-2

--- src/index.js
+++ src/index.js
@@ -3,9 +3,49 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+function BoiilingVerdict(props){
+    if (props.celsius >= 100) {
+        return (
+            <p>The water would boil</p>
+        )
+    }
+    return (
+        <p>The water would NOT boil</p>
 
+    )
+}
 
+class Water extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            value: ''
+        };
+    }
+    handleChangeInput(event){
+        this.setState({
+            value: event.target.value
+        })
+    }
+render(){
+        return(
+            <fieldset>
+                <legend>dfv</legend>
+                <input
+                    type="text"
+                    onChange={this.handleChangeInput.bind(this)}
 
+                />
+                <BoiilingVerdict celsius={this.state.value}/>
+            </fieldset>
+        )
+}
+}
+
+ReactDOM.render(
+    <Water />,
+    document.getElementById('root')
+);
 
 
 
